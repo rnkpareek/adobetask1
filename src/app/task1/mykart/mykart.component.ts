cad727aeb4fc9cc07408e799cdb9e684c5e9897b
rnkpareek <ronakleo93@gmail.com>
2019-12-11 17:40:56 +0530

    subscribe value added

--- src/app/task1/mykart/mykart.component.ts
+++ src/app/task1/mykart/mykart.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Task1Service} from '../task1.service';
 @Component({
   selector: 'app-mykart',
   templateUrl: './mykart.component.html',
   styleUrls: ['./mykart.component.css']
 })
 export class MykartComponent implements OnInit {
-
-  constructor() { }
+  message:string;
+  constructor(private task1Service: Task1Service) { }
 
   ngOnInit() {
+    this.task1Service.currentMessage.subscribe(message => this.message = message)
   }
 
 }
