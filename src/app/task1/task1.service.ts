bc2c7d69367d4beef1592a504529c7363748f454
rnkpareek <ronakleo93@gmail.com>
2019-12-11 17:36:09 +0530

    service data added

--- src/app/task1/task1.service.ts
+++ src/app/task1/task1.service.ts
@@ -3,13 +3,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class Task1Service {
+    private messageSource = new BehaviorSubject<number>(0);
+        currentMessage = this.messageSource.asObservable();
     private getKart_URL="https://api.myjson.com/bins/qhnfp"
 
   constructor(private http: HttpClient) {
@@ -20,6 +22,8 @@ export class Task1Service {
     return this.http.get(this.getKart_URL);
   }
   
- 
+   changeCount(message: string) {
+      this.messageSource.next(message)
+    }
   
 }
