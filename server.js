33415b14f330bd9f071510aca3f22545b89e7490
Ronak Pandia <Ronak.Pandia@in.ey.com>
2019-12-11 11:42:39 +0530

    added version

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const path = require('path');
-
+const http = require('http');
 const app = express();
 
 
 app.use(express.static(__dirname + '/dist/kart'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/kart/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/kart/index.html'));
 });
-app.listen(3000,function(){
-    console.log('listening port 3000')
-  })
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
