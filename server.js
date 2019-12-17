46de9cb251dd11a7031d40e6b63063bd0b5d857a
Ronak Pandia <Ronak.Pandia@in.ey.com>
2019-12-17 16:05:54 +0530

    minnor issues

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const path = require('path');
-const http = require('http');
 const app = express();
 
-
+// Serve static files....
 app.use(express.static(__dirname + '/dist/kart'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/kart/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/kart/index.html'));
 });
 
-//Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log(`Running on localhost:${port}`));
-
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
