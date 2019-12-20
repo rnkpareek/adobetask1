a2b94339f6ab7ec67f41cf24c46cc717a507f866
Ronak Pandia <Ronak.Pandia@in.ey.com>
2019-12-20 15:42:27 +0530

    minnor in changes

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
-const express = require('express');
+/*const express = require('express');
+//const bodyParser = require('body-parser');
 const path = require('path');
+const http = require('http');
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/kart'));
+// API file for interacting with MongoDB
+//const api = require('./server/routes/api');
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/kart/index.html'));
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// API location
+app.use('/api', api);
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
+*/
+
+ const express = require('express');
+ const app = express();
+
+ app.use(express.static(__dirname + '/dist'));
+ app.listen(process.env.PORT || 8080);
